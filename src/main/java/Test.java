3a37064de1d99d566e1dbd650bb7f0cfcc136690
ls <2276922982@qq.com>
2023-04-26 15:48:13 +0800

    解决冲突。。。

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,11 +4,9 @@ public class Test {
         System.out.println("hello github!");
     }
     //演示冲突
-<<<<<<< HEAD
     //zs111
-=======
     //ls222
->>>>>>> ec21ec3d8a2f88c2eefeced7e12f6039f1fd72a6
+
     public static void main(String[] args) {
         method();
     }
