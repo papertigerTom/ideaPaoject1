eb16ab8e89965d996475857d3a2fc57d21126c14
ls <2276922982@qq.com>
2023-04-26 15:25:00 +0800

    第二次提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,10 @@
 public class Test {
-    public static void main(String[] args) {
+
+    public  static void method(){
         System.out.println("hello github!");
+    }
+
+    public static void main(String[] args) {
+        method();
     }
 }
